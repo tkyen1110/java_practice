107c078523b37b27a2c2922b50c816cde60cad7c
Tk.Yen <tk.yen@advantech.com.tw>
2021-08-23 13:58:59 +0800

    Add 01_Getting_Started/helloworldapp

---
+++ 01_Getting_Started/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldapp;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java_practice
+
+Reference: https://docs.oracle.com/javase/tutorial/index.html
